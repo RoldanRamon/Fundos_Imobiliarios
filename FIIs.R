e90226e35b82b89ee0778e6c73fc4fb39de50513
Ramon Roldan de Lara <roldanramon83@gmail.com>
2023-04-02 13:14:44 -0300

    add all files

---
+++ FIIs.R
@@ -0,0 +1,48 @@
+rm(list = ls())
+library(dplyr)
+library(ggplot2)
+library(rvest)
+library(janitor)
+library(readr)
+library(stringr)
+#library(mailR)
+
+# Url da página do Fundamentus
+fiis_url <- 'https://fundamentus.com.br/fii_resultado.php'
+
+# Lendo a tabela da página
+fiis <- read_html(fiis_url) %>% 
+  html_table() %>% 
+  data.frame() %>% 
+  clean_names() %>% 
+  mutate(cotacao = parse_number(cotacao, locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         p_vp = parse_number(p_vp, locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         valor_de_mercado = parse_number(valor_de_mercado, locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         liquidez = parse_number(liquidez, locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         preco_do_m2 = parse_number(preco_do_m2, locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         aluguel_por_m2 = parse_number(aluguel_por_m2, locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         ffo_yield = str_replace_all(ffo_yield, '%', '') %>% parse_number(locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         dividend_yield = str_replace_all(dividend_yield, '%', '') %>% parse_number(locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         cap_rate = str_replace_all(cap_rate, '%', '') %>% parse_number(locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         vacancia_media = str_replace_all(vacancia_media, '%', '') %>% parse_number(locale = locale(decimal_mark = ',', grouping_mark = '.')),
+         across(where(is.character), as.factor),
+         data_atualizacao = format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+  )
+
+# Escrevendo o arquivo em disco
+write.csv(fiis, file = "fiis.csv", row.names = FALSE)
+
+# # Enviando o arquivo por e-mail
+# send.mail(from = "carlitosperini@yahoo.com",
+#           to = "roldanramon83@gmail.com",
+#           subject = "Arquivo de FIIs",
+#           body = "Segue em anexo o arquivo de FIIs atualizado.",
+#           smtp = list(host.name = "smtp.mail.yahoo.com",
+#                       port = 587,
+#                       user.name = "carlitosperini@yahoo.com",
+#                       passwd = "camarada10",
+#                       tls = TRUE,
+#                       ssl = FALSE),
+#           #authenticate = TRUE,
+#           send = TRUE,
+#           attach.files = "fiis.csv")
