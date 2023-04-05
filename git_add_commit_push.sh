6a1c6b5e4f4ccdf64525af343b8aac2f48d5ca75
Ramon Roldan de Lara <roldanramon83@gmail.com>
2023-04-04 21:50:13 -0300

    update 11 all files

--- git_add_commit_push.sh
+++ git_add_commit_push.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-echo "Updating the total dataset"
-
-if [[ "$(git status --porcelain)" != "" ]]; then
-    git config --global user.name 'RoldanRamon'
-    git config --global user.email 'roldanramon83@gmail.com'
-    git add .
-    git commit -m "Auto update Dataset"
-    git push
-fi
+touch camarada.txt
