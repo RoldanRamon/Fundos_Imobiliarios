3304edd4b320111b32456f442b4c6c31913f001c
Ramon Roldan de Lara <roldanramon83@gmail.com>
2023-10-05 22:20:02 -0300

    update dash

--- myapp/app.R
+++ myapp/app.R
@@ -1,59 +1,159 @@
 library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(plotly)
+library(DT)
 
-# Define UI for app that draws a histogram ----
-ui <- fluidPage(
-
-  # App title ----
-  titlePanel("Hello Shiny!"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-
-    ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-
+# Define UI
+ui <- dashboardPage(
+  dashboardHeader(title = "Exemplo Shiny"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Clusterização", tabName = "cluster", icon = icon("chart-area")),
+      selectInput("species", "Selecionar Espécie:", choices = unique(iris$Species))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "dashboard",
+        fluidRow(
+          valueBoxOutput("valuebox1"),
+          valueBoxOutput("valuebox2"),
+          valueBoxOutput("valuebox3"),
+          valueBoxOutput("valuebox4")
+        ),
+        hr(),
+        fluidRow(
+          box(
+            title = "Filtro de Hambúrguer",
+            width = 12,
+            background = "yellow"
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Gráfico de Barras 1",
+            width = 6,
+            plotOutput("barplot1")
+          ),
+          box(
+            title = "Gráfico de Barras 2",
+            width = 6,
+            plotOutput("barplot2")
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Gráfico de Linha",
+            width = 12,
+            plotOutput("lineplot")
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Heatmap",
+            width = 12,
+            plotlyOutput("heatmap")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "cluster",
+        fluidRow(
+          box(
+            title = "Gráfico de Clusterização (k-means)",
+            width = 12,
+            plotOutput("kmeans_plot")
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Estatísticas Descritivas",
+            width = 6,
+            tableOutput("desc_stats")
+          ),
+          box(
+            title = "Base de Dados",
+            width = 6,
+            DTOutput("data_table")
+          )
+        )
+      )
     )
   )
 )
 
-# Define server logic required to draw a histogram ----
+# Define server
 server <- function(input, output) {
-
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-
-    })
-
+  # ValueBoxes
+  output$valuebox1 <- renderValueBox({
+    valueBox(42, "Valor 1", icon = icon("heart"))
+  })
+  
+  output$valuebox2 <- renderValueBox({
+    valueBox(123, "Valor 2", icon = icon("chart-bar"))
+  })
+  
+  output$valuebox3 <- renderValueBox({
+    valueBox(876, "Valor 3", icon = icon("money-bill-alt"))
+  })
+  
+  output$valuebox4 <- renderValueBox({
+    valueBox(456, "Valor 4", icon = icon("tachometer-alt"))
+  })
+  
+  # Gráficos
+  output$barplot1 <- renderPlot({
+    data <- iris[iris$Species == input$species, ]
+    ggplot2::ggplot(data, aes(x = Species, fill = Species)) +
+      ggplot2::geom_bar() +
+      ggplot2::labs(title = "Gráfico de Barras 1")
+  })
+  
+  output$barplot2 <- renderPlot({
+    data <- iris[iris$Species == input$species, ]
+    ggplot2::ggplot(data, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+      ggplot2::geom_point() +
+      ggplot2::labs(title = "Gráfico de Barras 2")
+  })
+  
+  output$lineplot <- renderPlot({
+    data <- iris[iris$Species == input$species, ]
+    ggplot2::ggplot(data, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+      ggplot2::geom_line() +
+      ggplot2::labs(title = "Gráfico de Linha")
+  })
+  
+  output$heatmap <- renderPlotly({
+    data <- cor(iris[, 1:4])
+    plotly::plot_ly(z = data, colorscale = "Viridis") %>%
+      plotly::layout(title = "Heatmap")
+  })
+  
+  # Clusterização com k-means
+  output$kmeans_plot <- renderPlot({
+    data <- iris[, 1:4]
+    kmeans_result <- kmeans(data, centers = 3)
+    data$cluster <- as.factor(kmeans_result$cluster)
+    ggplot(data, aes(x = Sepal.Length, y = Sepal.Width, color = cluster)) +
+      geom_point() +
+      labs(title = "Gráfico de Clusterização (k-means)")
+  })
+  
+  # Estatísticas Descritivas
+  output$desc_stats <- renderTable({
+    data <- iris[iris$Species == input$species, ]
+    summary(data)
+  })
+  
+  # Tabela de Dados
+  output$data_table <- renderDT({
+    data <- iris[iris$Species == input$species, ]
+    datatable(data)
+  })
 }
 
-# Create Shiny app ----
-shinyApp(ui = ui, server = server)
+# Run the Shiny app
+shinyApp(ui, server)
